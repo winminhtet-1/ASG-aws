1726264f31106bf4deb95e13b012921489ad428a
Win Min Htet <winminhtet@2c2p.com>
2023-08-14 16:58:51 +0700

    update

--- terraform-aws/modules/alb/main.tf
+++ terraform-aws/modules/alb/main.tf
@@ -17,6 +17,9 @@ resource "aws_alb" "application_load_balancer" {
     var.tags
   )
 }
+module "alb_module" {
+  source = "../../../modules/alb"  # Adjust the path accordingly
+}
 
 resource "aws_alb_target_group" "alb_tg" {
   name_prefix = "alb-tg"
