4e201011b7055c541ed41a37da60af50c5212ec0
Win Min Htet <winminhtet@2c2p.com>
2023-08-14 21:07:50 +0700

    update

--- terraform-aws/modules/asg/main.tf
+++ terraform-aws/modules/asg/main.tf
@@ -76,5 +76,5 @@ resource "aws_autoscaling_policy" "cpu_scaling_policy" {
 
 resource "aws_autoscaling_attachment" "application_asg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.application_asg.name
-  lb_target_group_arn    = var.alb_target_group_arn
+  alb_target_group_arn   = var.alb_target_group_arn
 }
